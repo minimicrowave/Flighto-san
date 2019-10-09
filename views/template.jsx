2a58aa9cae6f7c5ff807278f4822c82afbc1cdca
Serene <unexpecte-d@hotmail.com>
2019-10-10 03:27:34 +0800

    Remove all jsx errors from template

--- views/template.jsx
+++ views/template.jsx
@@ -6,9 +6,9 @@ render() {
         <html>
         <head>
             <link rel="stylesheet" type="text/css" href="/style.css"/>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"></link>
             <link href="https://fonts.googleapis.com/css?family=Unica+One|Zilla+Slab|Open+Sans+Condensed:300" rel="stylesheet"></link>
-            <meta charset="UTF-8"/>
+            <meta charSet="UTF-8"/>
         </head>
         <body>
         <nav className="navbar fixed-top navbar-light bg-light topbar">
@@ -16,11 +16,11 @@ render() {
             <img className="logo" src="https://static1.squarespace.com/static/5aec99592971146b8ee7df33/t/5aecbf89575d1f6cb17212eb/1525464980165/animat-paper-airplane-color.gif"/>
                 Flighto-San
             </a>
-            <span class="navbar-text">
+            <span className="navbar-text">
                 <h6 id="name"></h6>
-                <a class="btn btn-outline-secondary btn-sm logoutbtn" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Edit Profile</a>
+                <a className="btn btn-outline-secondary btn-sm logoutbtn" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Edit Profile</a>
 
-                <a href="/logout" class="btn btn-outline-secondary btn-sm logoutbtn">Logout</a>
+                <a href="/logout" className="btn btn-outline-secondary btn-sm logoutbtn">Logout</a>
             </span>
         </nav>
         <main>
@@ -30,7 +30,7 @@ render() {
                     <input name="flightNo" className="form-control flight" type="search" placeholder="Flight No."/>
                     <button id="submitbutton" className="btn btn-small btn-block btn-outline-light my-2 my-sm-0" type="submit"><img className="openbtnimg" src="https://images.vexels.com/media/users/3/132068/isolated/preview/f9bb81e576c1a361c61a8c08945b2c48-search-icon-by-vexels.png"/>  Search</button>
                 </form>
-            <table class="table table-light table-hover">
+            <table className="table table-light table-hover">
                 <tbody>
            
                     {this.props.results}
@@ -56,29 +56,29 @@ render() {
         </main>
             <script type="text/javascript" src="/script.js"/>
             <script type="text/javascript" src="/scriptMaps.js"/>
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossOrigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossOrigin="anonymous"></script>
             <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaQfI1KG7e34IftdKC6ysqKP0zUKugaFc"></script>
         </body>
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Change Name</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Change Name</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
+                <div className="modal-body">
                     <form action="/profile/edit?_method=PUT" method="POST"> 
-                    <div class="form-group">
-                        <label for="recipient-name" class="col-form-label">New Name:</label>
-                        <input name="newname" type="text" class="form-control" id="recipient-name"/>
+                    <div className="form-group">
+                        <label htmlFor="recipient-name" className="col-form-label">New Name:</label>
+                        <input name="newname" type="text" className="form-control" id="recipient-name"/>
                     </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-info">Save Changes</button>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" className="btn btn-info">Save Changes</button>
                 </div>
                     </form>
                 </div>
